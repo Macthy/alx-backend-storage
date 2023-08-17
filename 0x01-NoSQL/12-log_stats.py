6eee34e62ffcb07e8dff1fbb797b01fc5a1d6591
thymac <thywillberdone@gmail.com>
2023-08-17 11:20:52 -0700

    revised

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,25 +2,25 @@
 """ Log stats """
 from pymongo import MongoClient
 
-def print_stats():
-    client = MongoClient('mongodb://localhost:27017/')
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
     logs_db = client.logs
     nginx_collection = logs_db.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     # Count total number of logs
     total_logs = nginx_collection.count_documents({})
-
     print(f"{total_logs} logs")
 
     # Count methods
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+        count_method = nginx_collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {count_method}")
 
     # Count status check with method=GET and path=/status
-    status_check_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check_count} status check")
+    status_check_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
 
-if __name__ == "__main__":
-    print_stats()
+    print(f"{status_check_count} status check")
